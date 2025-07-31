0c356020ebb087a0d2ad6a313f3c991781028441
danito89 <146493874+danito89@users.noreply.github.com>
2025-07-30 22:59:24 -0300

    .

--- Modulo 1/01 funcion print.py
+++ Modulo 1/01 funcion print.py
@@ -7,6 +7,10 @@ print(1)
 print(1.5,"abc") """
 
 print ("¡Hola, mundo!") #Imprime un mensaje en la pantalla
+#resultado: ¡Hola, mundo!
 print (1) #Imprime un número entero
+#resultado: 1
 print (1.5, "abc") #Imprime un número decimal y una cadena de texto
-print (1, 2, 3, sep="**") #Imprime varios números separados
+#resultado: 1.5 abc
+print (1, 2, 3, sep="**") #Imprime varios números separados (en este caso utilizo ** como separador, como podria haber usado una coma o un espacio)
+#resultado: 1**2**3
