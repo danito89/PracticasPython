b4d3d898133d0ca9953718019f4acd1f6d97e13e
danito89 <146493874+danito89@users.noreply.github.com>
2025-08-01 21:04:08 -0300

    .

---
+++ ejercicios.py
@@ -0,0 +1,13 @@
+""" Escribir un programa que tenga dos variables nombradas a y b con valores numéricos cualesquiera e 
+   imprima el cociente entre la suma y la resta de dichas variables.
+     Ej:
+    
+   a=5
+   b=3
+   4.0"""
+a=5
+b=3
+
+cociente = (a + b) / (a - b)
+print(cociente)
+#resultado 4.0
